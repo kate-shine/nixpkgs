db18650ddfb3cae14186e8de2d55fb7ed0547fcb
jdb <mail@jdb.sh>
2020-05-14 08:21:52 +0100

    tanka: init at 0.10.0

---
+++ pkgs/applications/networking/cluster/tanka/default.nix
@@ -0,0 +1,23 @@
+{ stdenv, buildGoModule, fetchFromGitHub }:
+
+buildGoModule rec {
+  pname = "tanka";
+  version = "0.10.0";
+
+  src = fetchFromGitHub {
+    owner = "grafana";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "02nm56iyxvyfyz6l150nqqy9dsn5r03hvmjqm9xxvs2bqlqfpnqy";
+  };
+
+  modSha256 = "1lxmhvflvln8h2xz759zql71mgjmmnagzrbnqhyg2vvgrdglf999";
+
+  meta = with stdenv.lib; {
+    description = ''
+      Flexible, reusable and concise configuration for Kubernetes https://tanka.dev
+    '';
+    license = licenses.asl20;
+    maintainers = with maintainers; [ jdbaldry ];
+  };
+}
